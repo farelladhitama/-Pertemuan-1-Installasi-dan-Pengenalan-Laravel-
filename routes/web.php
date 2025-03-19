a9f74a0358da6f8ab5944371a7c3d098b4d27d58
Farell Pria Adhitama <farelladhitama@gmail.com>
2025-03-20 00:55:44 +0700

    first comit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+// Menampilkan "Hello, World!" di halaman utama Laravel
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Nama : Farell Pria Adhitama NIM : 23090125.   E-Commerce (Electronic Commerce) adalah sistem perdagangan elektronik yang memungkinkan transaksi jual beli barang atau jasa melalui internet. Dalam e-commerce, pengguna dapat menjelajahi produk, menambahkannya ke keranjang belanja, dan melakukan pembayaran secara online. Platform ini biasanya mencakup fitur seperti katalog produk, sistem pembayaran, pengelolaan pesanan, serta keamanan transaksi. E-commerce mempermudah pelanggan dalam berbelanja kapan saja dan di mana saja tanpa harus mengunjungi toko fisik.';
+});
